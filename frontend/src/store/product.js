1039e4eab695384f77ebd44fd60d47bd0b0edb17
Mory <Mory@gmail.com>
2025-09-24 15:36:27 +0300

    successfully updated the url for backend to connect with the backend

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -1,5 +1,8 @@
 import { create } from "zustand";
 
+const API_URL =
+  import.meta.env.VITE_REACT_APP_BACKEND_BASEURL || "http://localhost:5000";
+
 export const useProductStore = create((set) => ({
   products: [],
   setProducts: (products) => set({ products }),
@@ -7,7 +10,8 @@ export const useProductStore = create((set) => ({
     if (!newProduct.name || !newProduct.price || !newProduct.image) {
       return { success: false, message: "Please Fill All Fields." };
     }
-    const res = await fetch("/api/products", {
+
+    const res = await fetch(`${API_URL}/api/products`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -19,12 +23,12 @@ export const useProductStore = create((set) => ({
     return { success: true, message: "Product was Created Successfully!" };
   },
   fetchProducts: async () => {
-    const res = await fetch("/api/products");
+    const res = await fetch(`${API_URL}/api/products`);
     const data = await res.json();
     set({ products: data.data });
   },
   deleteProduct: async (productId) => {
-    const res = await fetch(`/api/products/${productId}`, {
+    const res = await fetch(`${API_URL}/api/products/${productId}`, {
       method: "DELETE",
     });
     const data = await res.json();
@@ -35,7 +39,7 @@ export const useProductStore = create((set) => ({
     return { success: true, message: data.message };
   },
   updateProduct: async (productId, updateProduct) => {
-    const res = await fetch(`/api/products/${productId}`, {
+    const res = await fetch(`${API_URL}/api/products/${productId}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
