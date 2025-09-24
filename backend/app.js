c8ae36b7358c07d3c19ea2ab6920a8e8b4ce62a3
Mory <Mory@gmail.com>
2025-09-24 19:27:00 +0300

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -30,14 +30,6 @@ app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
-
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port: ${PORT}`);
